1703859aff408e849334b30e01011fc2cd728928
Code-Name-0 <ra.djebarra@esi-sba.dz>
2022-04-11 05:54:18 -0700

    change

--- src/components/testing/test.jsx
+++ src/components/testing/test.jsx
@@ -1,5 +1,7 @@
 const Test = () => {
-    return ( <h1>This is a test</h1> );
+    return ( <h1>This iszsazsazdazzadzadzadaz 
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore ab tenetur, delectus esse voluptates doloribus sint dolorum atque suscipit. Praesentium nam sint laborum commodi exercitationem dicta animi soluta tempora aliquid.</p>
+    </h1> );
 }
  
 export default Test;
