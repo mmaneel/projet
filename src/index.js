539ede2617c53f6b138237485949b77b56e34084
Code-Name-0 <ra.djebarra@esi-sba.dz>
2022-04-11 05:41:08 -0700

    initiale

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import HomePage from './components/homePage/HomePage'
 import {LandingPage} from './components/LandingPage/index'
- 
+import Test from './components/testing/test';
+
+
 
 ReactDOM.render(
-  <HomePage />,
+  <Test />,
   document.getElementById('root')
-  );
+);
   
 
 
